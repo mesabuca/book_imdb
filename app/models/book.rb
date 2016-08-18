acf4cdc69a178330a6a5bb4535fd16f92bacc9dc
GamzeAkbal1 <gakbal112@gmail.com>
2016-08-18 14:44:34 +0300

    #6 Set Relation between Book and Author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
 	has_many  :votes
+	belongs_to :author
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818111035) do
+ActiveRecord::Schema.define(version: 20160818114034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20160818111035) do
     t.string   "publisher"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id", using: :btree
   end
 
   create_table "members", force: :cascade do |t|
@@ -64,17 +66,17 @@ ActiveRecord::Schema.define(version: 20160818111035) do
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true, using: :btree
   end
 
-
   create_table "votes", force: :cascade do |t|
     t.integer  "point"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "book_id"
     t.integer  "member_id"
-    t.index["book_id"], name: "index_votes_on_book_id", using: :btree
+    t.index ["book_id"], name: "index_votes_on_book_id", using: :btree
     t.index ["member_id"], name: "index_votes_on_member_id", using: :btree
   end
 
+  add_foreign_key "books", "authors"
   add_foreign_key "votes", "books"
   add_foreign_key "votes", "members"
 end
