f1639ec39400f016c5f28b95b77ffb9d2ac0aae5
BerkanAcar <berkanacar93@gmail.com>
2016-08-19 18:44:45 +0300

    edit book page link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818125200) do
+ActiveRecord::Schema.define(version: 20160819082528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20160818125200) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "author_id"
+    t.integer  "admin_id"
     t.index ["admin_id"], name: "index_books_on_admin_id", using: :btree
     t.index ["author_id"], name: "index_books_on_author_id", using: :btree
   end
