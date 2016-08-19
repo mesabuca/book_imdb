aafd8d0a91015c574f25482c40eb882130980c46
GamzeAkbal1 <gakbal112@gmail.com>
2016-08-19 11:36:10 +0300

    #12 Created Rent's controller

---
+++ app/controllers/rents_controller.rb
@@ -0,0 +1,19 @@
+class RentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'rents/index'
+
+  get 'rents/show'
+
+  get 'rents/destroy'
+
+  get 'rents/edit'
+
+  get 'rents/update'
+
+  get 'rents/new'
+
   root to: 'welcome#index'
   resources :books
   devise_for :admins
