968906ccdfa831096d4e4d012ef81294e9164c17
BerkanAcar <berkanacar93@gmail.com>
2016-08-19 11:53:10 +0300

    #8 add comment controllers and form html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :comments
   root to: 'welcome#index'
   resources :books do
   	resources :votes
+  	resources :comments
   end
   devise_for :admins
   devise_for :members
