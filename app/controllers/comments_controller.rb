040ce17a915d9625505b59ceea9d974a55bf34fb
BerkanAcar <berkanacar93@gmail.com>
2016-08-19 15:18:38 +0300

    #8 edit controller and form partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,9 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @book, notice: 'Comment was deleted.'
   end
 
   private
