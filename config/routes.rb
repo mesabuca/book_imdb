79aade400e85baa65723b33dc9bd7311614b7745
BerkanAcar <berkanacar93@gmail.com>
2016-08-22 13:26:18 +0300

    edit conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 
   resources :books do
-    resources :rents
+    #resources :rents
   	resources :comments
   	resources :votes, only: [:create, :update]
     collection do
@@ -17,18 +17,12 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
   resources :authors
-  get 'votes/index'
-  get 'authors/index'
-
-
-<<<<<<< HEAD
-
+  #get 'votes/index'
+  #get 'authors/index'
   resources :categories, only: [:show, :index]
-
-=======
 resources :categories
 resources :rents
->>>>>>> 5a90e8f95c8480b346d0430c3692ea9a4efde801
+
 
 
 end
